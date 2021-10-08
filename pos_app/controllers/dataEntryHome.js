9d25150a6321eb08dd996e9fefae094dc4a761bb
Josh Love <joshdavidlove@gmail.com>
2021-10-08 08:37:44 -0400

    Added JSON reader

--- pos_app/controllers/dataEntryHome.js
+++ pos_app/controllers/dataEntryHome.js
@@ -1,6 +1,13 @@
+// Demo JSON
+const fs = require('fs');
+
+const sellers = JSON.parse(fs.readFileSync('./data/sellers.json', 'utf8'));
+
+console.log(sellers);
+
 // Display homepage
 const dataEntryHome = (req, res) => {
-    res.render('dataEntryHome');
+    res.render('dataEntryHome', {sellers});
 };
 
 module.exports = {dataEntryHome}
